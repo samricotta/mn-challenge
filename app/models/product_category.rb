5ac82677096d95e8ab8e42392d5c9a67cf273fc4
Sam Ricotta <samanthalricotta@gmail.com>
2018-08-09 11:40:18 +0100

    models created, migrated, associations created and started the seeds file

---
+++ app/models/product_category.rb
@@ -0,0 +1,4 @@
+class ProductCategory < ApplicationRecord
+  belongs_to :cateory
+  belongs_to :product
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_09_094241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
